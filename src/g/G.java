8b40ad7526d0a01cecae60c49c11452738fb6255
kazimdaqiqi <155591206+kazimdaqiqi@users.noreply.github.com>
2024-01-07 12:34:24 -0500

    Update G.java
    Add

--- src/g/G.java
+++ src/g/G.java
@@ -4,5 +4,7 @@ public class G {
 	public static void main (String args[]) {
 		System.out.println("Kazim Daqiqi.........");
 	}
+	public static void add(int a, int b)
+	System.out.println(a+b); 
 
 }
